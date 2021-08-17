2750532eb00b57e818d3cdecf8ab6e5c74a1e2e7
Millennium <glc2266@gmail.com>
2021-08-17 15:47:35 -0400

    Recommendation system algorithm + data

---
+++ recommendation-alg/strategies/common.js
@@ -0,0 +1,32 @@
+const similarity = require('compute-cosine-similarity')
+
+ function sortByScore(recommendation) {
+  return recommendation.sort((a, b) => b.score - a.score);
+}
+
+// X x 1 row vector based on similarities of movies
+// 1 equals similar, -1 equals not similar, 0 equals orthogonal
+// Whole matrix is too computational expensive for 45.000 movies
+// https://en.wikipedia.org/wiki/Cosine_similarity
+ function getCosineSimilarityRowVector(matrix, index) {
+  return matrix.map((rowRelative, i) => {
+    return similarity(matrix[index], matrix[i]);
+  });
+}
+
+ function getMovieIndexByTitle(MOVIES_IN_LIST, query) {
+  const index = MOVIES_IN_LIST.map(movie => movie.title).indexOf(query);
+
+  if (!index) {
+    throw new Error('Movie not found');
+  }
+
+  const { title, id } = MOVIES_IN_LIST[index];
+  return { index, title, id };
+}
+
+module.exports = {
+  getMovieIndexByTitle,
+  getCosineSimilarityRowVector,
+  sortByScore
+}
