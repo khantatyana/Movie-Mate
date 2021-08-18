bf33d3103b30468f4368e87f35b049f126d20a77
Millennium <glc2266@gmail.com>
2021-08-18 08:13:59 -0500

    Added route for recommendations

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const moviesRoute = require("./movies");
 const usersRoute = require("./users");
+const recsRoute = require("./recommendations")
 
 const routesSetup = (app) => {
   app.use("/movies", moviesRoute);
   app.use("/users", usersRoute);
+  app.use("/recs", recsRoute)
 
   app.use("*", (req, res) => {
     res.sendStatus(404);
