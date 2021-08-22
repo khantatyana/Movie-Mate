1e6287c27525a0a2440e549137a73dfc04905528
Ayman Elkfrawy <elkfrawy@gmail.com>
2021-08-22 11:19:41 -0700

    Remove recommendation route module

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const moviesRoute = require("./movies");
 const usersRoute = require("./users");
-const recsRoute = require("./recommendations")
 
 const routesSetup = (app) => {
   app.use("/api/movies", moviesRoute);
   app.use("/api/users", usersRoute);
-  app.use("/api/recs", recsRoute)
 
   app.use("*", (req, res) => {
     res.sendStatus(404);
