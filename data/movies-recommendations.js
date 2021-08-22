12a444cc4409b2fa894b072e6ce6c582dfe1dd32
Millennium <glc2266@gmail.com>
2021-08-22 09:11:05 -0400

    Updated getRecommendations

--- data/movies-recommendations.js
+++ data/movies-recommendations.js
@@ -6,7 +6,7 @@ const { movies } = require(".");
 async function getRecommendations(likedMovies) {
   let recommendedMovies = await getRecommendations(likedMovies, 20);
   recommendedMovies = recommendedMovies.map((v) => {
-    movies.getMovieById(v);
+    await movies.getMovieById(v);
   });
   /* Mock data
   const recommendedMovies = [
@@ -16,24 +16,7 @@ async function getRecommendations(likedMovies) {
       year: 2021,
       posterUrl: "/szcH4JUFQfL9jc26jETOepiq9NU.jpg",
     },
-    {
-      _id: 254460,
-      title: "Jungle Cruise",
-      year: 2021,
-      posterUrl: "/9dKCd55IuTT5QRs989m9Qlb7d2B.jpg",
-    },
-    {
-      _id: 233661,
-      title: "The Courier",
-      year: 2021,
-      posterUrl: "/zFIjKtZrzhmc7HecdFXXjsLR2Ig.jpg",
-    },
-    {
-      _id: 244334,
-      title: "Wrath of Man",
-      year: 2021,
-      posterUrl: "/M7SUK85sKjaStg4TKhlAVyGlz3.jpg",
-    },
+    ...
   ];*/
 
   parentPort.postMessage(recommendedMovies);
