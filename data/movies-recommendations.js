8779a74c2c460635b7ed50fce72467af0fc9f260
Millennium <glc2266@gmail.com>
2021-08-22 16:54:38 -0400

    Updated movies-recommendations.js

--- data/movies-recommendations.js
+++ data/movies-recommendations.js
@@ -33,14 +33,14 @@ async function getRecommendations() {
     movieInfo = [];
     for (let rec of recs) {
       if (typeof rec !== "undefined" || res !== null) {
-        // let m = await movies.getMovieById(Number.parseInt(rec.movie.id));
-        let m = await movies.getMovieById(rec.movie.id);
+        let m = await movies.getMovieById(Number.parseInt(rec.movie.id));
+        //let m = await movies.getMovieById(rec.movie.id);
         if (m) {
           //console.log(typeof rec.movie.id + ": " + rec.movie.id);
           movieInfo.push(m);
         } else {
-          //m = await movielens.getSimilarMovies(rec.movie.id, 1)
-          //movieInfo.concat(m);
+          m = await movielens.getSimilarMovies(rec.movie.id, 1);
+          movieInfo.concat(m);
         }
       }
     }
@@ -50,8 +50,8 @@ async function getRecommendations() {
     console.log(movieInfo);
     recommendations = movieInfo;
   } catch (e) {
-    //let m = await movielens.getSimilarMovies(ids[0], 20)
-    //recommendations = m
+    let m = await movielens.getSimilarMovies(ids[0], 20);
+    recommendations = m;
   }
   parentPort.postMessage(recommendations);
 }
